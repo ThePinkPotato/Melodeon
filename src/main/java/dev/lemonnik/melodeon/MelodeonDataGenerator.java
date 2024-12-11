5411f024a0cae285cd615af4acc81b1fe4fd2df4
Lemonnik6484 <LemonGamer6484@gmail.com>
2024-12-11 20:47:17 +0100

    init

---
+++ src/main/java/dev/lemonnik/melodeon/MelodeonDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.lemonnik.melodeon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MelodeonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
