6e88f2e33a3e817b12ce9e73ea4330a5e0d076cf
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-02-25 00:21:30 +0100

    it's working perfectly

--- src/main/java/dev/lemonnik/melodeon/MelodeonClient.java
+++ src/main/java/dev/lemonnik/melodeon/MelodeonClient.java
@@ -4,9 +4,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
@@ -15,48 +18,125 @@ import net.minecraft.world.biome.Biome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static net.minecraft.client.sound.PositionedSoundInstance.record;
-
 public class MelodeonClient implements ClientModInitializer {
     public static final String MOD_ID = "melodeon";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    private RegistryKey<Biome> currentBiomeKey;
+    private static final int CHECK_INTERVAL = 0;
+    private int tickCounter = 0;
+    private Identifier lastBiomeId = null;
+    private SoundInstance currentAmbientSound;
 
     @Override
     public void onInitializeClient() {
         CommandRegistrationCallback.EVENT.register(Commands::register);
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.world != null && client.player != null) {
-                RegistryEntry<Biome> biomeEntry = client.world.getBiome(client.player.getBlockPos());
-                RegistryKey<Biome> newBiomeKey = biomeEntry.getKey().orElse(null);
-                String biomeModName = newBiomeKey.getValue().getNamespace();
-
-                if (!newBiomeKey.equals(currentBiomeKey)) {
-                    if (currentBiomeKey != null) {
-                        playBiomeSound("leave", currentBiomeKey, biomeModName);
-                    }
-                    playBiomeSound("enter", newBiomeKey, biomeModName);
-                    currentBiomeKey = newBiomeKey;
-                } else {
-                    playBiomeSound("ambient", newBiomeKey, biomeModName);
-                }
-            }
-        });
+        ClientTickEvents.END_CLIENT_TICK.register(this::handleBiomeMusic);
     }
 
-    private void playBiomeSound(String type, RegistryKey<Biome> biomeKey, String modName) {
-        String biomeName = biomeKey.getValue().getPath();
-        Identifier soundId = new Identifier(Melodeon.MOD_ID, type + "." + modName + "." + biomeName);
-        SoundEvent soundEvent = MinecraftClient.getInstance().world.getRegistryManager()
-                .get(RegistryKeys.SOUND_EVENT).get(soundId);
+    private void handleBiomeMusic(MinecraftClient client) {
+        if (client.world == null || client.player == null) return;
 
+        if (tickCounter++ >= CHECK_INTERVAL) {
+            tickCounter = 0;
+            RegistryEntry<Biome> biomeEntry = client.world.getBiome(client.player.getBlockPos());
+            Identifier currentBiomeId = biomeEntry.getKey().map(RegistryKey::getValue).orElse(null);
+
+            if (currentBiomeId != null && !currentBiomeId.equals(lastBiomeId)) {
+                handleBiomeChange(client, currentBiomeId);
+            }
+        }
+    }
+
+    private void handleBiomeChange(MinecraftClient client, Identifier newBiomeId) {
+        if (lastBiomeId != null) {
+            playLeaveSound(client, lastBiomeId);
+            stopCurrentAmbient(client);
+        }
+
+        playEnterSound(client, newBiomeId);
+        playAmbientSound(client, newBiomeId);
+
+        lastBiomeId = newBiomeId;
+    }
+
+    private void playLeaveSound(MinecraftClient client, Identifier biomeId) {
+        Identifier soundId = new Identifier(MOD_ID, "leave." + biomeId.getNamespace() + "." + biomeId.getPath());
+        SoundEvent soundEvent = getSoundEvent(client, soundId);
         if (soundEvent != null) {
-            Vec3d playerPos = MinecraftClient.getInstance().player.getPos();
-            MinecraftClient.getInstance().getSoundManager().play(record(
-                    soundEvent, playerPos
-            ));
+            Vec3d pos = client.player.getPos();
+            SoundInstance sound = new PositionedSoundInstance(
+                    soundEvent.getId(),
+                    SoundCategory.AMBIENT,
+                    1.0F,
+                    1.0F,
+                    SoundInstance.createRandom(),
+                    false,
+                    0,
+                    SoundInstance.AttenuationType.LINEAR,
+                    pos.x,
+                    pos.y,
+                    pos.z,
+                    false
+            );
+            client.getSoundManager().play(sound);
+        }
+    }
+
+    private void playEnterSound(MinecraftClient client, Identifier biomeId) {
+        Identifier soundId = new Identifier(MOD_ID, "enter." + biomeId.getNamespace() + "." + biomeId.getPath());
+        SoundEvent soundEvent = getSoundEvent(client, soundId);
+        if (soundEvent != null) {
+            Vec3d pos = client.player.getPos();
+            SoundInstance sound = new PositionedSoundInstance(
+                    soundEvent.getId(),
+                    SoundCategory.AMBIENT,
+                    1.0F,
+                    1.0F,
+                    SoundInstance.createRandom(),
+                    false,
+                    0,
+                    SoundInstance.AttenuationType.LINEAR,
+                    pos.x,
+                    pos.y,
+                    pos.z,
+                    false
+            );
+            client.getSoundManager().play(sound);
+        }
+    }
+
+    private void playAmbientSound(MinecraftClient client, Identifier biomeId) {
+        Identifier soundId = new Identifier(MOD_ID, "ambient." + biomeId.getNamespace() + "." + biomeId.getPath());
+        SoundEvent soundEvent = getSoundEvent(client, soundId);
+        if (soundEvent != null) {
+            currentAmbientSound = new PositionedSoundInstance(
+                    soundEvent.getId(),
+                    SoundCategory.AMBIENT,
+                    1.0F,
+                    1.0F,
+                    SoundInstance.createRandom(),
+                    true,
+                    0,
+                    SoundInstance.AttenuationType.NONE,
+                    0.0,
+                    0.0,
+                    0.0,
+                    true
+            );
+            client.getSoundManager().play(currentAmbientSound);
+        }
+    }
+
+    private SoundEvent getSoundEvent(MinecraftClient client, Identifier soundId) {
+        return client.world.getRegistryManager()
+                .get(RegistryKeys.SOUND_EVENT)
+                .get(soundId);
+    }
+
+    private void stopCurrentAmbient(MinecraftClient client) {
+        if (currentAmbientSound != null) {
+            client.getSoundManager().stop(currentAmbientSound);
+            currentAmbientSound = null;
         }
     }
 }
